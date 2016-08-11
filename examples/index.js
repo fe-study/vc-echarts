de51b39663940819a06af1faf07688444291ab83
YuAoi <zibo.z.liu@gmail.com>
2016-08-11 15:18:32 +0800

    初始化指令

---
+++ examples/index.js
@@ -0,0 +1,68 @@
+import Vue from 'vue'
+import { VcEcharts } from '../dist/build.js'
+
+Vue.use(VcEcharts)
+
+let chartOptions = {
+    title : {
+        text: '饼图程序调用高亮示例',
+        x: 'center'
+    },
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+    },
+    series : [
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius : '55%',
+            center: ['50%', '60%'],
+            data:[
+                {value:335, name:'直接访问'},
+                {value:310, name:'邮件营销'},
+                {value:234, name:'联盟广告'},
+                {value:135, name:'视频广告'},
+                {value:1548, name:'搜索引擎'}
+            ],
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+    ]
+}
+
+new Vue({
+    el: '#app',
+    data () {
+        return {
+            chartOptions: {},
+            ns: 'mc',
+            receiveEvents: ['click', 'mouseout'],
+            loading: true,
+        }
+    },
+    events: {
+        ['mc.click'] (params) {
+            console.log(params)
+        },
+        ['mc.mouseout'] (params) {
+            console.log(params)
+        }
+    },
+    created () {
+        setTimeout(() => {
+            this.chartOptions = chartOptions
+            this.loading = false
+        }, 2000)
+    }
+})
